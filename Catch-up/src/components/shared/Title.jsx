ba63bfc3af12b620b6a1a6b6ea097408712816a0
Utkarsh Alshi <117593889+UtkarshAlshi@users.noreply.github.com>
2024-06-19 19:44:12 +0530

    Add files via upload

---
+++ Catch-up/src/components/shared/Title.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+const Title = ({
+  title = "ChitChat",
+  description = "this is the Chat App called ChitChat",
+}) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+    </Helmet>
+  );
+};
+
+export default Title;
